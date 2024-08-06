2c706b6bac5e869957a46ce99b7cc09f8948c59c
Javi Palanca <jpalanca@gmail.com>
2024-08-06 21:27:21 +0200

    Update ugettext_lazy to gettext_lazy

--- likert_field/forms.py
+++ likert_field/forms.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.forms import fields
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .widgets import LikertTextField
 
